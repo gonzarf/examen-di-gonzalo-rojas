141fa8a1e1acc201a3aaa5b1a98b5f3618164210
gonzarf <gonzarf21@gmail.com>
2023-12-11 11:30:18 +0100

    ActivityMain

---
+++ app/src/main/java/com/example/gonzalorojas/SplashScreen.java
@@ -0,0 +1,52 @@
+package com.example.gonzalorojas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        openLogin();
+
+        ImageView logoSplash = findViewById(R.id.logoSplash);
+        TextView appName = findViewById(R.id.nombreAppSplash);
+
+        Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logoSplash.startAnimation(fadeAnimation);
+        appName.startAnimation(fadeAnimation);
+
+        ImageView background = findViewById(R.id.backgroundSplash);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1535905557558-afc4877a26fc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(3000))
+                .centerCrop()
+                .into(background);
+    }
+
+    public void openLogin(){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+            }
+        }, 3000);
+    }
+}
